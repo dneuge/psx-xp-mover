5394b1d82cb71535ddcb1f726635771b5d651469
Daniel Neugebauer <dneuge@energiequant.de>
2024-11-23 15:15:52 +0100

    threads_macos: print name of error codes

--- src/threads_macos.c
+++ src/threads_macos.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "utils.h"
 
@@ -17,27 +18,27 @@ int mtx_init(mtx_t *mutex, int type) {
 	pthread_mutexattr_t attr = {0};
 	err = pthread_mutexattr_init(&attr);
 	if (err) {
-		printf("[threads_macos] pthread_mutexattr_init error: %d\n", err);
+		printf("[threads_macos] pthread_mutexattr_init error: %d %s\n", err, strerror(err));
 		return thrd_error;
 	}
 	
 	if (type & mtx_recursive) {
 		err = pthread_mutexattr_settype(&attr, PTHREAD_MUTEX_RECURSIVE);
 		if (err) {
-			printf("[threads_macos] pthread_mutexattr_settype PTHREAD_MUTEX_RECURSIVE error: %d\n", err);
+			printf("[threads_macos] pthread_mutexattr_settype PTHREAD_MUTEX_RECURSIVE error: %d %s\n", err, strerror(err));
 			goto end;
 		}
 	}
 	
 	err = pthread_mutex_init(mutex, &attr);
 	if (err) {
-		printf("[threads_macos] pthread_mutex_init error: %d\n", err);
+		printf("[threads_macos] pthread_mutex_init error: %d %s\n", err, strerror(err));
 	}
 
 end:
 	fin_err = pthread_mutexattr_destroy(&attr);
 	if (fin_err) {
-		printf("[threads_macos] pthread_mutexattr_destroy error: %d\n", fin_err);
+		printf("[threads_macos] pthread_mutexattr_destroy error: %d %s\n", fin_err, strerror(fin_err));
 	}
 	
 	return err ? thrd_error : thrd_success;
@@ -46,14 +47,14 @@ end:
 void mtx_destroy(mtx_t *mutex) {
 	int err = pthread_mutex_destroy(mutex);
 	if (err) {
-		printf("[threads_macos] pthread_mutex_destroy error: %d\n", err);
+		printf("[threads_macos] pthread_mutex_destroy error: %d %s\n", err, strerror(err));
 	}
 }
 
 int mtx_lock(mtx_t *mutex) {
 	int err = pthread_mutex_lock(mutex);
 	if (err) {
-		printf("[threads_macos] pthread_mutex_lock error: %d\n", err);
+		printf("[threads_macos] pthread_mutex_lock error: %d %s\n", err, strerror(err));
 	}
 
 	return err ? thrd_error : thrd_success;
@@ -62,7 +63,7 @@ int mtx_lock(mtx_t *mutex) {
 int mtx_unlock(mtx_t *mutex) {
 	int err = pthread_mutex_unlock(mutex);
 	if (err) {
-		printf("[threads_macos] pthread_mutex_unlock error: %d\n", err);
+		printf("[threads_macos] pthread_mutex_unlock error: %d %s\n", err, strerror(err));
 	}
 
 	return err ? thrd_error : thrd_success;
@@ -92,7 +93,7 @@ int thrd_create(thrd_t *thr, thrd_start_t func, void *arg) {
 
 	int err = pthread_create(thr, NULL, wrap_thread_func, wrapped);
 	if (err) {
-		printf("[threads_macos] pthread_create error: %d\n", err);
+		printf("[threads_macos] pthread_create error: %d %s\n", err, strerror(err));
 		free(wrapped);
 	}
 
@@ -108,7 +109,7 @@ int thrd_join(thrd_t thr, int *res) {
 	wrapped_thread_t *wrapped = NULL;
 	int err = pthread_join(thr, (void**) &wrapped);
 	if (err) {
-		printf("[threads_macos] pthread_join error: %d\n", err);
+		printf("[threads_macos] pthread_join error: %d %s\n", err, strerror(err));
 		return thrd_error;
 	}
 
